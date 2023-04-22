a5fdeea11cb1b95a8b587919a97d6786c5aadaae
Patrick McCaffrey <1471088+McCaffeteria@users.noreply.github.com>
2023-04-22 02:17:19 -0400

    First person 6 degree freedom movement implemented

--- Prototypes/Dynamic Gravity Prototype v01/Scripts/Player.gd
+++ Prototypes/Dynamic Gravity Prototype v01/Scripts/Player.gd
@@ -1,4 +1,46 @@
 extends RigidBody3D
 
+var move_speed = 3 #meters per second, average walking speed is 1.4
+var look_speed = 1.5 #radians
+var multiplier = 100
+
+var input_dir = Vector3.ZERO
+var output_dir_basis = Basis()
+var output_dir = Vector3.ZERO
+var input_rot = Vector3.ZERO
+var output_rot_basis = Basis()
+var output_rot = Vector3.ZERO
+
 func _physics_process(delta):
 	apply_central_force(get_node("..").calc_grav(self))
+	
+	#Local refference
+	input_dir = Vector3.ZERO
+	input_rot = Vector3.ZERO
+	input_dir.x -= Input.get_action_strength("move_left")
+	input_dir.x += Input.get_action_strength("move_right")
+	input_dir.y -= Input.get_action_strength("move_down")
+	input_dir.y += Input.get_action_strength("move_up")
+	input_dir.z -= Input.get_action_strength("move_forward")
+	input_dir.z += Input.get_action_strength("move_backward")
+	input_dir = input_dir * move_speed * multiplier
+	input_rot.x -= Input.get_action_strength("look_down")
+	input_rot.x += Input.get_action_strength("look_up")
+	input_rot.y -= Input.get_action_strength("look_right")
+	input_rot.y += Input.get_action_strength("look_left")
+	input_rot.z -= Input.get_action_strength("roll_right")
+	input_rot.z += Input.get_action_strength("roll_left")
+	input_rot = input_rot * look_speed * multiplier
+	
+	output_dir_basis.x = input_dir.x * transform.basis.x
+	output_dir_basis.y = input_dir.y * transform.basis.y
+	output_dir_basis.z = input_dir.z * transform.basis.z
+	output_dir = output_dir_basis.x + output_dir_basis.y + output_dir_basis.z
+	output_rot_basis.x = input_rot.x * transform.basis.x
+	output_rot_basis.y = input_rot.y * transform.basis.y
+	output_rot_basis.z = input_rot.z * transform.basis.z
+	output_rot = output_rot_basis.x + output_rot_basis.y + output_rot_basis.z
+	
+	#parent refference
+	apply_central_force(output_dir)
+	apply_torque(output_rot)
